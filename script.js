0c8a0b52b30c0c1dd08e974c0c9ebcc1f8521885
Irene Couto <irenecoutto@gmail.com>
2023-06-02 21:27:05 -0300

    product list

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 //função de mudar de tela
 
 function login() {
@@ -9,124 +8,143 @@ function logout() {
   window.location.href = '../index.html'
 }
 
-function navigateToAdminPage() { 
+function navigateToAdminPage() {
   window.location.href = 'home-admin.html'
 }
 
-function navigateToHome() {  
-   console.log("star")
+function navigateToHome() {
+  console.log('star')
   window.location.href = 'home.html'
-
 }
 
-function navigateToProductRegistration() { 
+function navigateToProductRegistration() {
   window.location.href = 'product-registration.html'
 }
 
-function navigateToListPage() { 
+function navigateToListPage() {
   window.location.href = 'list.html'
 }
 
-function navigateToDevolutionPage() { 
+function navigateToDevolutionPage() {
   window.location.href = 'devolution.html'
 }
 
 function toggleModal(tableRow) {
-  const dialog= document.getElementById("list-modal")
-  const description= document.getElementById("description")
-  const img= document.getElementById("product-img")
-  description.innerText=tableRow.getAttribute("data-description"); 
-  img.src=tableRow.getAttribute("data-imagem")
-  dialog.showModal();
+  const dialog = document.getElementById('list-modal')
+  const description = document.getElementById('description')
+  const img = document.getElementById('product-img')
+  description.innerText = tableRow.getAttribute('data-description')
+  img.src = tableRow.getAttribute('data-imagem')
+  dialog.showModal()
 }
 //função de pesquisar por materiais
 
-document.addEventListener('DOMContentLoaded', function() {
-  const tabelaMateriais = document.getElementById('tabela-materiais');
-  const inputPesquisa = document.getElementById('input-pesquisa');
+function toggleModaal(element) {
+  const description = element.getAttribute('data-description')
+  const imageSrc = element.getAttribute('data-imagem')
 
-  inputPesquisa.addEventListener('input', function(event) {
-    const termoPesquisa = event.target.value.toLowerCase();
-    const linhasTabela = tabelaMateriais.getElementsByTagName('tr');
+  const descriptionElement = document.getElementById('description')
+  const imageElement = document.getElementById('product-img')
+
+  descriptionElement.textContent = description
+  imageElement.src = imageSrc
+
+  const modal = document.getElementById('list-modal')
+  modal.showModal()
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const tabelaMateriais = document.getElementById('tabela-materiais')
+  const inputPesquisa = document.getElementById('input-pesquisa')
+
+  inputPesquisa.addEventListener('input', function (event) {
+    const termoPesquisa = event.target.value.toLowerCase()
+    const linhasTabela = tabelaMateriais.getElementsByTagName('tr')
 
     for (let i = 0; i < linhasTabela.length; i++) {
-      const colunas = linhasTabela[i].getElementsByTagName('td');
-      let correspondenciaEncontrada = false;
+      const colunas = linhasTabela[i].getElementsByTagName('td')
+      let correspondenciaEncontrada = false
 
       for (let j = 0; j < colunas.length; j++) {
-        const textoColuna = colunas[j].innerText.toLowerCase();
-        const palavrasPesquisa = termoPesquisa.split('*');
+        const textoColuna = colunas[j].innerText.toLowerCase()
+        const palavrasPesquisa = termoPesquisa.split('*')
 
-        let todasPalavrasEncontradas = true;
+        let todasPalavrasEncontradas = true
 
         for (let k = 0; k < palavrasPesquisa.length; k++) {
-          const palavra = palavrasPesquisa[k].trim();
+          const palavra = palavrasPesquisa[k].trim()
 
           if (palavra !== '' && !textoColuna.includes(palavra)) {
-            todasPalavrasEncontradas = false;
-            break;
+            todasPalavrasEncontradas = false
+            break
           }
         }
 
         if (todasPalavrasEncontradas) {
-          correspondenciaEncontrada = true;
-          break;
+          correspondenciaEncontrada = true
+          break
         }
       }
 
       if (correspondenciaEncontrada) {
-        linhasTabela[i].classList.remove('hide-row');
+        linhasTabela[i].classList.remove('hide-row')
       } else {
-        linhasTabela[i].classList.add('hide-row');
+        linhasTabela[i].classList.add('hide-row')
       }
     }
-  });
+  })
 
-  tabelaMateriais.addEventListener('click', function(event) {
-    const target = event.target;
+  tabelaMateriais.addEventListener('click', function (event) {
+    const target = event.target
 
     // Verifica se o clique foi em uma célula da tabela
     if (target.tagName === 'TD') {
-      const linhaSelecionada = target.parentNode;
+      const linhaSelecionada = target.parentNode
 
       // Remove a classe 'selected-row' de todas as linhas
-      const todasLinhas = tabelaMateriais.getElementsByTagName('tr');
+      const todasLinhas = tabelaMateriais.getElementsByTagName('tr')
       for (let i = 0; i < todasLinhas.length; i++) {
-        todasLinhas[i].classList.remove('selected-row');
+        todasLinhas[i].classList.remove('selected-row')
       }
 
       // Adiciona a classe 'selected-row' na linha selecionada
-      linhaSelecionada.classList.add('selected-row');
+      linhaSelecionada.classList.add('selected-row')
     }
-  });
-});
+  })
+})
 
-  function adicionarCarrinho() { 
-    alert("O item foi adicionado ao carrinho!");
-    window.location.href = 'list.html'
+function adicionarCarrinho() {
+  alert('O item foi adicionado ao carrinho!')
+  window.location.href = 'list.html'
 }
-
 
 //favoritos
 
 
-//função validar produto
 
 function cadastrarProduto() {
-  console.log("tty")
+  console.log('tty')
   // Obtendo os valores dos campos do formulário
-  var nome = document.getElementById("nome").value;
-  var codigo = document.getElementById("codigo").value;
-  var quantidade = document.getElementById("quantidade").value;
-  var tipo = document.getElementById("tipo").value;
-  var preco = document.getElementById("preco").value;
-  var descricao = document.getElementById("descricao").value;
-  var imagem = document.getElementById("imagem").value;
+  var nome = document.getElementById('nome').value
+  var codigo = document.getElementById('codigo').value
+  var quantidade = document.getElementById('quantidade').value
+  var tipo = document.getElementById('tipo').value
+  var preco = document.getElementById('preco').value
+  var descricao = document.getElementById('descricao').value
+  var imagem = document.getElementById('imagem').value
 
   // Validando os campos
-  if (nome === "" || codigo === "" || quantidade === "" || tipo === "" || preco === "" || descricao === "" || imagem === "") {
-    alert("Por favor, preencha todos os campos.");
-    return;
+  if (
+    nome === '' ||
+    codigo === '' ||
+    quantidade === '' ||
+    tipo === '' ||
+    preco === '' ||
+    descricao === '' ||
+    imagem === ''
+  ) {
+    alert('Por favor, preencha todos os campos.')
+    return
   }
 
   // Criando um objeto JSON com os dados do produto
@@ -138,33 +156,66 @@ function cadastrarProduto() {
     preco: preco,
     descricao: descricao,
     imagem: imagem
-  };
+  }
 
   // Verificando se já existem produtos armazenados em local storage
-  var produtosArmazenados = localStorage.getItem("produtos");
-  var produtos = [];
+  var produtosArmazenados = localStorage.getItem('produtos')
+  var produtos = []
 
   if (produtosArmazenados) {
     // Se existirem produtos armazenados, converte a string JSON para um array de objetos
-    produtos = JSON.parse(produtosArmazenados);
+    produtos = JSON.parse(produtosArmazenados)
   }
 
   // Adicionando o novo produto ao array
-  produtos.push(produto);
+  produtos.push(produto)
 
   // Armazenando o array atualizado em local storage
-  localStorage.setItem("produtos", JSON.stringify(produtos));
+  localStorage.setItem('produtos', JSON.stringify(produtos))
 
   // Limpa os campos do formulário após a validação
-  document.getElementById("nome").value = "";
-  document.getElementById("codigo").value = "";
-  document.getElementById("quantidade").value = "";
-  document.getElementById("tipo").value = "";
-  document.getElementById("preco").value = "";
-  document.getElementById("descricao").value = "";
-  document.getElementById("imagem").value = "";
+  document.getElementById('nome').value = ''
+  document.getElementById('codigo').value = ''
+  document.getElementById('quantidade').value = ''
+  document.getElementById('tipo').value = ''
+  document.getElementById('preco').value = ''
+  document.getElementById('descricao').value = ''
+  document.getElementById('imagem').value = ''
 
-  alert("Cadastro de produto realizado com sucesso!");
+  alert('Cadastro de produto realizado com sucesso!')
 }
 
+function atualizarListaProdutos() {
+  console.log('hjk')
+  var produtosArmazenados = localStorage.getItem('produtos')
+  var produtos = []
+
+  if (produtosArmazenados) {
+    produtos = JSON.parse(produtosArmazenados)
+  }
+
+  var tabelaBody = document.getElementById('produtos-lista')
+
+  // Limpa o conteúdo atual da tabela
+
+  // Percorre o array de produtos e adiciona cada um na tabela
+  produtos.forEach(function (produto) {
+    var novaLinha = document.createElement('tr')
+
+    novaLinha.innerHTML = `
+      <td>${produto.nome}</td>
+      <td>${produto.codigo}</td>
+      <td>${produto.quantidade}</td>
+      <td>${produto.tipo}</td>
+      <td>R$ ${produto.preco}</td>
+      <td>${produto.local}</td>
+      <td>
+        <span class="favoritos_list"><img src="../img/heart.png" alt=""></span>
+        <span class="carrinho_list"><img src="../img/carrinho2.png" alt=""></span>
+      </td>
+    `
+
+    tabelaBody.appendChild(novaLinha)
+  })
+}
 
