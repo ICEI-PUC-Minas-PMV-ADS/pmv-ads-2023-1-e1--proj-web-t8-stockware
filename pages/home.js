b3d7ac7596f65576ba18e8a14ddcb4e9b3fba358
richard.monteiro@sga.pucminas.br <richard.monteiro@sga.pucminas.br>
2023-06-04 21:26:31 -0300

    Adicionado função para obter URL da imagem e corrigido rota ADMIN

--- pages/home.js
+++ pages/home.js
@@ -1,7 +1,7 @@
 const sidebar = document.querySelector(".main-content");
 const btnSideBar = document.querySelector(".btn-sidebar");
 
-btnSideBar.addEventListener('click', ()=> {
+btnSideBar.addEventListener('click', () => {
     sidebar.classList.toggle('active');
 })
 
