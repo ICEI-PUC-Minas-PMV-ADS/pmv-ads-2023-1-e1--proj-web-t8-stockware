6b31474601f684e7ae0497642211e935cb28f8a9
Marcos36523 <marcos.dias.1457746@sga.pucminas.br>
2023-06-07 19:37:46 -0300

    Atualizações de layout

---
+++ login.js
@@ -0,0 +1,50 @@
+
+function login() {
+    var username = document.getElementById("usernameInput").value;
+    var password = document.getElementById("passwordInput").value;
+
+    // Lista de usuários permitidos
+    var usuariosPermitidos = [
+        { username: "usuario1", password: "senha1", nome: "João" },
+        { username: "usuario2", password: "senha2", nome: "Maria" },
+        { username: "usuario3", password: "senha3", nome: "Pedro" },
+        { username: "admin", password: "admin", nome: "ADM" }
+        
+    ];
+    if (
+username === '' ||
+password === ''
+
+) {
+alert('Por favor, preencha todos os campos.')
+return
+}
+
+    var isUserAllowed = false;
+    var isAdmin = false;
+
+    for (var i = 0; i < usuariosPermitidos.length; i++) {
+        if (usuariosPermitidos[i].username === username && usuariosPermitidos[i].password === password) {
+            isUserAllowed = true;
+            break;
+        }
+    }
+
+    if (isUserAllowed) {
+        if (username === "admin") {
+            isAdmin = true;
+        }
+
+        if (isAdmin) {
+            alert("Login bem-sucedido como admin!");
+            // Redireciona para a tela de admin
+            window.location.href = "pages/home-admin.html?username=" + encodeURIComponent(username);
+        } else {
+            alert("Login bem-sucedido como usuário!");
+            // Redireciona para a tela de usuário
+            window.location.href = "pages/home.html?username=" + encodeURIComponent(username);
+        }
+    } else {
+        alert("Usuário ou senha inválidos. Tente novamente.");
+    }
+}
